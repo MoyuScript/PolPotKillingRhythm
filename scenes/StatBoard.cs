71aefbcce8f9cecc03267032f1942b2578a5d877
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2023-10-31 19:59:27 +0800

    计分板绑定状态

---
+++ scenes/StatBoard.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class StatBoard : Node
+{
+	int _currentScore = 0;
+	public override void _Ready()
+	{
+		var mainNode = GetNode<Main>("/root/Main");
+		var scoreLabelNode = GetNode<Label>("ScoreLabel");
+		var comboLabelNode = GetNode<Label>("ComboLabel");
+
+		mainNode.OnScoreChanged += () =>
+		{
+			var tween = CreateTween();
+			tween.TweenMethod(
+				Callable.From(
+					(int val) =>
+					{
+						scoreLabelNode.Text = $"分数：{val:D8}";
+						_currentScore = val;
+					}
+				),
+				_currentScore,
+				mainNode.Score,
+				0.5
+			);
+
+		};
+
+		mainNode.OnComboChanged += () =>
+		{
+			comboLabelNode.Text = mainNode.Combo.ToString();
+		};
+	}
+}
