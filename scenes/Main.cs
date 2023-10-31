0505ac5256eb89a31c0d2a68d041b94442d541dd
MoyuScript <moyuscript@gmail.com>
2023-10-31 19:59:27 +0800

    计分板绑定状态

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -4,16 +4,55 @@ using System;
 public partial class Main : Node2D
 {
 	bool _isPlaying;
-	public bool IsPlaying { get => _isPlaying; }
+	public bool IsPlaying
+	{
+		get => _isPlaying;
+		private set
+		{
+			_isPlaying = value;
+			EmitSignal(SignalName.OnIsPlayingChanged);
+		}
+	}
 	[Signal] public delegate void OnIsPlayingChangedEventHandler();
 
 	bool _isContinuouslyNote;
-	public bool IsContinuouslyNote { get => _isContinuouslyNote; }
+	public bool IsContinuouslyNote
+	{
+		get => _isContinuouslyNote;
+		private set
+		{
+			_isContinuouslyNote = value;
+			EmitSignal(SignalName.OnIsContinuouslyNoteChanged);
+		}
+	}
 	[Signal] public delegate void OnIsContinuouslyNoteChangedEventHandler();
+
+	int _score;
+	public int Score
+	{
+		get => _score;
+		private set
+		{
+			_score = value;
+			EmitSignal(SignalName.OnScoreChanged);
+		}
+	}
+	[Signal] public delegate void OnScoreChangedEventHandler();
+
+	int _combo;
+	public int Combo {
+		get => _combo;
+		private set
+		{
+			_combo = value;
+			EmitSignal(SignalName.OnComboChanged);
+		}
+	}
+	[Signal] public delegate void OnComboChangedEventHandler();
 
 	public override void _Process(double delta)
 	{
-		if (!_isPlaying && Input.IsActionJustPressed("key_space"))
+		if (!IsPlaying && Input.IsActionJustPressed("key_space"))
 		{
 			StartGame();
 		}
@@ -21,13 +60,13 @@ public partial class Main : Node2D
 
 	public void StartGame()
 	{
-		if (_isPlaying)
+		if (IsPlaying)
 		{
 			return;
 		}
 
-		_isPlaying = true;
-		EmitSignal(SignalName.OnIsPlayingChanged);
+		IsPlaying = false;
+
 		GetNode<TextureRect>("BackUI/TextStart").Visible = false;
 		GetNode<AudioStreamPlayer>("BackgroundMusic").Play();
 	}
@@ -38,10 +77,8 @@ public partial class Main : Node2D
 		audioStreamPlayer.Stop();
 		audioStreamPlayer.Seek(0);
 
-		_isPlaying = false;
-		EmitSignal(SignalName.OnIsPlayingChanged);
-		_isContinuouslyNote = false;
-		EmitSignal(SignalName.OnIsContinuouslyNoteChanged);
+		IsPlaying = false;
+		IsContinuouslyNote = false;
 	}
 }
 
